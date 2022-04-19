e6da2072dd944864f8bacc225a67d361988f8ad0
Kuro <runescapeshiro02@gmail.com>
2022-04-19 15:01:26 -0300

    alteração

---
+++ Lab_01/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Lab_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.tbxNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCurso = new System.Windows.Forms.Label();
+            this.tbxCurso = new System.Windows.Forms.ComboBox();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.Location = new System.Drawing.Point(294, 53);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(301, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Visualizar dados na caixa de texto";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbxNome
+            // 
+            this.tbxNome.Location = new System.Drawing.Point(327, 155);
+            this.tbxNome.Name = "tbxNome";
+            this.tbxNome.Size = new System.Drawing.Size(298, 23);
+            this.tbxNome.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.Location = new System.Drawing.Point(262, 155);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(59, 21);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "Nome:";
+            this.lblNome.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblCurso
+            // 
+            this.lblCurso.AutoSize = true;
+            this.lblCurso.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCurso.Location = new System.Drawing.Point(262, 197);
+            this.lblCurso.Name = "lblCurso";
+            this.lblCurso.Size = new System.Drawing.Size(56, 21);
+            this.lblCurso.TabIndex = 4;
+            this.lblCurso.Text = "Curso:";
+            // 
+            // tbxCurso
+            // 
+            this.tbxCurso.FormattingEnabled = true;
+            this.tbxCurso.Items.AddRange(new object[] {
+            "PHP",
+            "Java",
+            "JavaScript",
+            "Python",
+            "CSharp",
+            "Object_C",
+            "C++",
+            "C"});
+            this.tbxCurso.Location = new System.Drawing.Point(327, 194);
+            this.tbxCurso.Name = "tbxCurso";
+            this.tbxCurso.Size = new System.Drawing.Size(202, 23);
+            this.tbxCurso.TabIndex = 5;
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblMensagem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblMensagem.Location = new System.Drawing.Point(262, 293);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(363, 114);
+            this.lblMensagem.TabIndex = 6;
+            this.lblMensagem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(262, 247);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(363, 23);
+            this.btnExibir.TabIndex = 7;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 450);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.tbxCurso);
+            this.Controls.Add(this.lblCurso);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.tbxNome);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Primeiro Laboratóro ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox tbxNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCurso;
+        private System.Windows.Forms.ComboBox tbxCurso;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Button btnExibir;
+    }
+}
+
